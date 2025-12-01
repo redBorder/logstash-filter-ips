611827d9042ce6f00c68fa94325283ad1be22624
Pablo Pérez <pperez@redborder.com>
2025-12-01 11:50:05 +0000

    refactor enrich_ip_scores

--- lib/logstash/filters/store/aerospike_store.rb
+++ lib/logstash/filters/store/aerospike_store.rb
@@ -67,23 +67,7 @@ class AerospikeStore
 
       score_src, score_dst = -1
 
-      if !data_src.empty?
-        score_src = data_src[SCORE]
-        data_src.delete(SCORE)
-        list_type_src = data_src[LIST_TYPE]
-        data_src.delete(LIST_TYPE)
-
-        unless list_type_src.nil?
-          if list_type_src == 'black'
-            score_src = 100
-          elsif list_type_src == 'white'
-            score_src = 0
-          end
-          data["ip_#{LIST_TYPE}"] = list_type_src
-        else
-          data["ip_#{LIST_TYPE}"] = 'none'
-        end
-      else
+      if data_src.empty?
         score_src = -1
         params = {}
         params['http'] = 'asynchronous'
@@ -91,25 +75,24 @@ class AerospikeStore
         params['ip'] = src
 
         Manticore.post(make_random_reputation_url, body: params.to_json.to_s).body
+      else
+        score_src = data_src['score']
+        data_src.delete('score')
+        list_type_src = data_src['list_type']
+        data_src.delete('list_type')
+
+        if list_type_src.nil?
+          data["ip_#{LIST_TYPE}"] = 'none'
+        elsif list_type_src == 'black'
+          score_src = 100
+          data["ip_#{LIST_TYPE}"] = list_type_src
+        elsif list_type_src == 'white'
+          score_src = 0
+          data["ip_#{LIST_TYPE}"] = list_type_src
+        end
       end
 
-      if !data_dst.nil?
-        score_dst = data_dst[SCORE]
-        data_dst.delete(SCORE)
-        list_type_dst = data_dst[LIST_TYPE]
-        data_dst.delete(LIST_TYPE)
-
-        unless list_type_dst.nil?
-          if list_type_dst == 'black'
-            score_dst = 100
-          elsif list_type_dst == 'white'
-            score_dst = 0
-          end
-          data["ip_#{LIST_TYPE}"] = list_Type_dst
-        else
-          data["ip_#{LIST_TYPE}"] = 'none'
-        end
-      else
+      if data_dst.nil?
         score_dst = -1
         params = {}
         params['http'] = 'asynchronous'
@@ -117,26 +100,42 @@ class AerospikeStore
         params['ip'] = dst
 
         Manticore.post(make_random_reputation_url, body: params.to_json.to_s).body
+      else
+        score_dst = data_dst['score']
+        data_dst.delete('score')
+        list_type_dst = data_dst['list_type']
+        data_dst.delete('list_type')
+
+        if list_type_dst.nil?
+          data["ip_#{LIST_TYPE}"] = 'none'
+        else
+          if list_type_dst == 'black'
+            score_dst = 100
+          elsif list_type_dst == 'white'
+            score_dst = 0
+          end
+          data["ip_#{LIST_TYPE}"] = list_Type_dst
+        end
       end
 
       score_src = -1 unless score_src
       score_dst = -1 unless score_dst
 
       if (score_src > 0 && score_dst > 0)
-        data[IP_DIRECTION] = 'both'
+        data['ip_direction'] = 'both'
         if score_src > score_dst
-          data["ip_#{SCORE}"]  = score_src
+          data["ip_#{SCORE}"] = score_src
         else
           data["ip_#{SCORE}"] = score_dst
         end
       elsif score_src > 0
-        data[IP_DIRECTION] = 'source'
+        data['ip_direction'] = 'source'
         data["ip_#{SCORE}"] = score_src
       elsif score_dst > 0
-        data[IP_DIRECTION] = 'destination'
+        data['ip_direction'] = 'destination'
         data["ip_#{SCORE}"] = score_dst
       else
-        data[IP_DIRECTION] = 'none'
+        data['ip_direction'] = 'none'
         data["ip_#{SCORE}"] = -1
       end
 
@@ -144,9 +143,9 @@ class AerospikeStore
       data_src = @aerospike.get(src_key).bins rescue {}
 
       unless data_src.empty?
-        score_src = data_src[SCORE]
-        data_src.delete(SCORE)
-        list_type_src = data_src[LIST_TYPE]
+        score_src = data_src['score']
+        data_src.delete('score')
+        list_type_src = data_src['list_type']
         # TODO: we dont need to delete list_type_src??
         unless list_type_src.nil?
           if list_type_src == 'black'
@@ -162,15 +161,15 @@ class AerospikeStore
         score_src = -1 unless score_src
 
         if (score_src > 0)
-          data[IP_DIRECTION] = 'source'
-          data["ip_"+SCORE] = score_src
+          data['ip_direction'] = 'source'
+          data["ip_" + SCORE] = score_src
         else
-          data[IP_DIRECTION] = 'none'
-          data["ip_"+SCORE] = -1
+          data['ip_direction'] = 'none'
+          data["ip_" + SCORE] = -1
         end
       else
-        data[IP_DIRECTION] = 'none'
-        data["ip_"+SCORE] = -1
+        data['ip_direction'] = 'none'
+        data["ip_" + SCORE] = -1
 
         params = {}
         params['http'] = 'asynchronous'
@@ -189,35 +188,13 @@ class AerospikeStore
 
     elsif !dst.nil?
       data_dst = @aerospike.get(dst_key).bins rescue {}
-      score_dst = data_dst[SCORE] rescue nil
-      data_dst.delete(SCORE)
-      list_type_src = data_dst[LIST_TYPE]
-      data_dst.delete(LIST_TYPE)
+      score_dst = data_dst['score'] rescue nil
+      data_dst.delete('score')
+      list_type_src = data_dst['list_type']
+      data_dst.delete('list_type')
 
-      unless data_dst.empty?
-        unless list_type_src.nil?
-          if list_type_src == 'black'
-            score_dst = 100
-          elsif list_type_src == 'white'
-            score_dst = 0
-          end
-          data["ip_#{LIST_TYPE}"] = list_type_src
-        else
-          data["ip_#{LIST_TYPE}"] = 'none'
-        end
-
-        score_dst = -1 unless score_dst
-
-        if score_dst > 0
-          data[IP_DIRECTION] = 'destionation'
-          data["ip_#{SCORE}"] = score_dst
-        else
-          data[IP_DIRECTION] = 'none'
-          data["ip_#{SCORE}"] = -1
-        end
-
-      else
-        data[IP_DIRECTION] = 'none'
+      if data_dst.empty?
+        data['ip_direction'] = 'none'
         data["ip_#{SCORE}"] = -1
 
         params = {}
@@ -226,6 +203,26 @@ class AerospikeStore
         params['ip'] = dst
 
         Manticore.post(make_random_reputation_url, body: params.to_json.to_s).body
+      else
+        if list_type_src.nil?
+          data["ip_#{LIST_TYPE}"] = 'none'
+        elsif list_type_src == 'black'
+          score_dst = 100
+          data["ip_#{LIST_TYPE}"] = list_type_src
+        elsif list_type_src == 'white'
+          score_dst = 0
+          data["ip_#{LIST_TYPE}"] = list_type_src
+        end
+
+        score_dst = -1 unless score_dst
+
+        if score_dst > 0
+          data['ip_direction'] = 'destionation'
+          data["ip_#{SCORE}"] = score_dst
+        else
+          data['ip_direction'] = 'none'
+          data["ip_#{SCORE}"] = -1
+        end
       end
     end
     data
